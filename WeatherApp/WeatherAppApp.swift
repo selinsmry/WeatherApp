d0bbea5ced846bee9578f0eca3388fd08bdf1a2e
selinsmry <selinsamray12@gmail.com>
2025-07-03 21:29:45 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Selin Samray on 3.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherAppTests/WeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppTests.swift
+//  WeatherAppTests
+//
+//  Created by Selin Samray on 3.07.2025.
+//
+
+import Testing
+@testable import WeatherApp
+
+struct WeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
